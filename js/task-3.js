b1e9619d8179ae7fa3974dcca934150ec9977a25
Borys Ziukov <ziukoff1985@gmail.com>
2024-08-30 11:18:23 +0300

    added js files and solving of tasks 1, 2, 3, 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,21 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  const contentNumber = Number.parseFloat(content);
-  const paddingNumber = Number.parseFloat(padding);
-  const borderNumber = Number.parseFloat(border);
-  const width = contentNumber + paddingNumber * 2 + borderNumber * 2;
-  return width;
+function checkForSpam(message) {
+  const messageToLowerCase = message.toLowerCase();
+  if (
+    messageToLowerCase.includes('spam') ||
+    messageToLowerCase.includes('sale')
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
